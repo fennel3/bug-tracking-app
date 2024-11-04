4097327cc4a1b684d273c482d6961530aedbf666
chris <landwaterfood@gmail.com>
2024-11-04 15:34:21 +0000

    .

--- api/issues.php
+++ api/issues.php
@@ -10,5 +10,3 @@ $issues = IssueHydrator::getIssues($db);
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
 echo json_encode($issues);
-
-git commit -m"moved issues.php to /api"
