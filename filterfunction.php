e5fd48f4646b4bf8765c2450e1a249e9ff8b917c
chris <landwaterfood@gmail.com>
2024-11-05 16:16:34 +0000

    .

---
+++ filterfunction.php
@@ -0,0 +1,35 @@
+<?php
+
+// I need to take the fields of the database and check the inputted entity against the
+//I may need to check i
+$new_issue= array(
+    'id' => ''
+    'title' =>
+    'description' =>
+    'severity' =>
+    'data_created' =>
+    'reporter' =>
+    'department' =>
+    'completed' =>
+
+);
+
+function filterLetterCount()
+{
+}
+
+
+filter_var_array(data_array)
+
+
+//CREATE TABLE `issues` (
+//`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+//  `title` varchar(255) NOT NULL,
+//  `description` text DEFAULT NULL,
+//  `severity` int(11) NOT NULL DEFAULT 0,
+//  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+//  `reporter` varchar(255) NOT NULL,
+//  `department` int(11) NOT NULL DEFAULT 0,
+//  `completed` int(1) NOT NULL DEFAULT 0,
+//  PRIMARY KEY (`id`)
+//)
--- report.php
+++ report.php
@@ -1,5 +1,7 @@
 <?php
 require('../vendor/autoload.php');
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
 
 use ITBugTracking\Factories\DatabaseConnector;
 use ITBugTracking\Hydrators\IssueHydrator;
@@ -7,6 +9,4 @@ use ITBugTracking\Hydrators\IssueHydrator;
 $db = DatabaseConnector::connect();
 $report = IssueHydrator::getIssues($db);
 
-header('Content-Type: application/json; charset=utf-8');
-header("Access-Control-Allow-Origin: *");
 echo json_encode($report);
