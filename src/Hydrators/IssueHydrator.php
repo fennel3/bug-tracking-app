207694109bc922b46b86ec423776c868f7bd32f7
jordan_brewer <jordanbrewer1994@gmail.com>
2024-11-05 10:31:55 +0000

    changed hydrator id name to issue_id

--- src/Hydrators/IssueHydrator.php
+++ src/Hydrators/IssueHydrator.php
@@ -7,10 +7,10 @@ use PDO;
 
 class IssueHydrator
 {
-    public static function getIssue($db, $id)
+    public static function getIssue($db, $issue_id)
     {
         $issueQuery = $db->prepare("SELECT * FROM `issues` WHERE `id` = :id");
-        $issueQuery->bindvalue(':id', $id, PDO::PARAM_INT);
+        $issueQuery->bindvalue(':id', $issue_id, PDO::PARAM_INT);
         $issueQuery->execute();
         $issueQuery->setFetchMode(PDO::FETCH_CLASS, Issue::class);
         return $issueQuery->fetchall();
