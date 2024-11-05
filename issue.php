ea11fd31f5ce0a73ffd4ce982452cfdb2734f85b
Matt_Mannings <matt.mannings97@gmail.com>
2024-11-05 15:08:35 +0000

    .

--- issue.php
+++ issue.php
@@ -8,4 +8,6 @@ $db = DatabaseConnector::connect();
 $issue = IssueHydrator::getIssue($db, $_GET['id']);
 
 header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+
 echo json_encode($issue);
