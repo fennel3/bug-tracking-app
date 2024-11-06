2306e442b5fe2733beb45ee13df4a10db0e60286
chris <landwaterfood@gmail.com>
2024-11-06 14:32:49 +0000

    validated, fields are entered, sanitized 255 char limit, checked decription exists and if 0 set to null, if not 0 than set to <10000 chars, set integers to integers, error codes included

--- src/Services/ValidationService.php
+++ src/Services/ValidationService.php
@@ -5,43 +5,59 @@ class ValidationService
 {
     public static function createIssue($db, $issue)
     {
+
         //validate if fields are entered
-        if (empty($issue['name']) ||empty($issue['title']) || empty($issue['description']) || empty($issue['severity']) || empty($issue['department']) ) {
-            throw new Exception('no data input');
+        if (empty($issue['name']) ||empty($issue['title']) || empty($issue['severity']) || empty($issue['department']) ) {
+            http_response_code(400 );
+            echo '"message": "Invalid issue data"';
+
         }
-        //saniti"z"e 255 character limits
+        //sanitiize 255 character limits
         $issue['name'] = htmlspecialchars(($issue['name']));
-        $issue['department'] = is_int(($issue['department']));
         $issue['title'] = htmlspecialchars(($issue['title']));
-        $issue['description'] = htmlspecialchars(($issue['description']));
-        $issue['severity'] = is_int(($issue['severity']));
 
-        if (strlen(($issue['name'] || $issue['title'])) < 0) {
-            echo "Input is too short, minimum is 0 characters (255 max).";
-        } elseif (strlen($issue['name'] || $issue['title']) > 255) {
-            echo "Input is too long, maximum is 255 characters.";
+        if (strlen($issue['name'] || $issue['title']) > 255) {
+            http_response_code(400 );
+            echo '"message": "Invalid issue data"';
         }
 
-        //and 100000 description character limit
-        if (strlen($issue['description']) > 10000) {
-            echo "Input is too long, maximum is 10000 characters.";
+        //and check if description exists, set to null or 100000 character limit
+        if (empty($issue['description'])) {
+            $issue['description'] = null;
+        } else{
+            if (strlen($issue['description']) > 10000) {
+                http_response_code(400 );
+                echo '"message": "Invalid issue data"';
+            }
         }
 
-        $severityNumQuery = $db->prepare('SELECT `id` FROM `severities` WHERE `name` = :severity;');
-        $severityNumQuery->execute([
-            'severity' => $issue['severity']
-        ]);
-
-        $severityNum = $severityNumQuery->fetch();
-
-        $query = $db->prepare('INSERT INTO `comments` (`name`) VALUES (:name); INSERT INTO `issues` (`department`, `title`, `description`, `severity`) VALUES (:department, :title, :description, :image, :severity)');
-        $query->execute([
-            'name' => $issue['name'],
-            'department' => $issue['department'],
-            'title' => $issue['title'],
-            'description' => $issue['description'],
-            'severity' => $severityNum
-        ]);
+        //check severity and department are integers
+            if(!filter_var($issue['severity'],FILTER_VALIDATE_INT)) {
+                http_response_code(400 );
+                echo '"message": "Invalid issue data"';
+            }
+            if(!filter_var($issue['department'],FILTER_VALIDATE_INT)){
+                http_response_code(400 );
+                echo '"message": "Invalid issue data"';
+            }
+//
+//        // Fetch severity ID if severity is provided as a name
+//        $severityNumQuery = $db->prepare('SELECT `id` FROM `severities` WHERE `name` = :severity;');
+//        $severityNumQuery->execute([
+//            'severity' => $issue['severity']
+//        ]);
+//
+//        $severityNum = $severityNumQuery->fetch();
+//
+//        $query = $db->prepare('INSERT INTO `comments` (`name`) VALUES (:name); INSERT INTO `issues` (`department`, `title`, `description`, `severity`) VALUES (:department, :title, :description, :image, :severity)');
+//        $query->execute([
+//            'name' => $issue['name'],
+//            'department' => $issue['department'],
+//            'title' => $issue['title'],
+//            'description' => $issue['description'],
+//            'severity' => $severityNum
+//        ]);
+        echo http_response_code(201);
         return $db->lastInsertId();
-    }
+            }
 }
