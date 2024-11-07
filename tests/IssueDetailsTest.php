8d965adaecf3545c0be9517d78d9e535ab7fbcf6
Matt_Mannings <matt.mannings97@gmail.com>
2024-11-07 10:03:53 +0000

    serialize success test working. issue tests failingin this branch

---
+++ tests/IssueDetailsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+use ITBugTracking\Entities\IssueDetails;
+use PHPUnit\Framework\TestCase;
+
+class IssueDetailsTest extends TestCase
+{
+
+    public function testJsonSerialize()
+    {
+        $issueDetails = new IssueDetails();
+
+        $issueDetails->id = 5;
+        $issueDetails->title = "A Title";
+        $issueDetails->summary = "This is a description.";
+        $issueDetails->severity = "Severe";
+        $issueDetails->date_created = "2024-11-05 15:56:55";
+        $issueDetails->comment_count = 5;
+        $issueDetails->reporter = "Joe Blogs";
+        $issueDetails->department = 1;
+        $issueDetails->comments = ["comment one", "comment two", "comment three"];
+
+        $expected = json_encode([
+            'id' => 5,
+            'title' => "A Title",
+            'severity' => "Severe",
+            'date_created' => "05/11/2024",
+            'comment_count' => 5,
+            'reporter' => "Joe Blogs",
+            'department' => 1,
+            'description' => "This is a description.",
+            'comments' => ["comment one", "comment two", "comment three"]
+        ]);
+
+        $actual = json_encode($issueDetails);
+
+        $this->assertEquals($expected, $actual);
+    }
+}
