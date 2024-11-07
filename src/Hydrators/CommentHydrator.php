f5925cfb8073f3688a410a6e2081260c4324a251
Matt_Mannings <matt.mannings97@gmail.com>
2024-11-07 13:18:19 +0000

    changed name of comment hydrator metod in lin with trainer review

--- src/Hydrators/CommentHydrator.php
+++ src/Hydrators/CommentHydrator.php
@@ -7,7 +7,7 @@ use PDO;
 
 class CommentHydrator
 {
-    public static function getComments($db, $issue_id)
+    public static function getCommentsOnIssue($db, $issue_id)
     {
         $query = $db->prepare("SELECT `name`, `comment`, DATE_FORMAT(`date_created`, '%d/%m/%Y %H:%i') AS 'date_created' FROM `comments` WHERE issue_id = :id;");
         $query->execute(['id' => $issue_id]);
