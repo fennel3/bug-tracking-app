ed843f1dd53239be271d7472a78aaafad7fe634d
Fred_Rice <fredjrice@outlook.com>
2024-11-08 12:39:43 +0000

    added try catch

--- comment.php
+++ comment.php
@@ -7,17 +7,37 @@ header("Access-Control-Allow-Origin: *");
 
 use ITBugTracking\Factories\DatabaseConnector;
 use ITBugTracking\Hydrators\CommentHydrator;
+use ITBugTracking\Services\ValidationService;
 
 $db = DatabaseConnector::connect();
 $issue_id = $_GET['id'];
 
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
-//TODO validation
+
+try{
 $newIssue = CommentHydrator::createComment($db, $data, $issue_id);
 
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
+
+
+
+$data['name'] = ValidationService::validateStringInput($data['name'], 255);
+
+if (isset($data['comment'])) {
+    $data['comment'] = ValidationService::validateStringInput($data['comment'], 65535);
+}
+
+$passedValidation =
+    $data['name']
+    && $data['comment'];
+
+if (!$passedValidation) {
+    http_response_code(400);
+    echo json_encode(["message" => "Invalid comment data"]);
+    return;
+}
 
 if ($newIssue) {
     $output = [
@@ -25,10 +45,14 @@ if ($newIssue) {
         'id' => $newIssue['id']
     ];
     http_response_code(201);
-} else {
+    echo json_encode($output);
+}
+} catch (Exception $e) {
     $output = [
         'message' => "Unexpected error",
     ];
     http_response_code(500);
+    echo json_encode($output);
 }
-echo json_encode($output);
+
+
