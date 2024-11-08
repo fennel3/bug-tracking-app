d128f0bf21c085335b91f85b0cb4e315edebece4
Liam O'Connell <liamslost@googlemail.com>
2024-11-08 12:07:21 +0000

    move catch to end of root in report

--- report.php
+++ report.php
@@ -15,14 +15,15 @@ $json = file_get_contents('php://input');
 
 $data = json_decode($json, true);
 
-$requiredExist = ValidationService::checkRequiredDataExists($data);
-
-if (!$requiredExist) {
-    http_response_code(400);
-    echo json_encode(["message" => "Invalid data"]);
-}
-
 try {
+    $requiredExist = ValidationService::checkRequiredDataExists($data);
+
+    if (!$requiredExist) {
+        http_response_code(400);
+        echo json_encode(["message" => "Invalid data"]);
+        return;
+    }
+
     $data['title'] = ValidationService::validateStringInput($data['title'], 100);
 
     $data['name'] = ValidationService::validateStringInput($data['name'], 255);
@@ -30,60 +31,55 @@ try {
     if (isset($data['description'])) {
         $data['description'] = ValidationService::validateStringInput($data['description'], 65535);
     }
-} catch (Exception $e) {
-    http_response_code(400);
-    echo json_encode(["message" => "Invalid description data"]);
-}
 
-$severityIsNumeric = is_numeric($data['severity']);
-if ($severityIsNumeric) {
-    $data['severity'] = (int)$data['severity'];
-    $severityExists = ValidationService::checkSeverityExists($db, $data['severity']);
-} else {
-    $severityExists = false;
-    http_response_code(400);
-    echo json_encode(["message" => "Invalid severity data"]);
-}
+    $severityIsNumeric = is_numeric($data['severity']);
+    if ($severityIsNumeric) {
+        $data['severity'] = (int)$data['severity'];
+        $severityExists = ValidationService::checkSeverityExists($db, $data['severity']);
+    } else {
+        $severityExists = false;
+        http_response_code(400);
+        echo json_encode(["message" => "Invalid severity data"]);
+    }
 
-$departmentIsNumeric = is_numeric($data['department']);
-if ($departmentIsNumeric) {
-    $data['department'] = (int)$data['department'];
-    $departmentExists = ValidationService::checkDepartmentExists($db, $data['department']);
-} else {
-    $departmentExists = false;
-    http_response_code(400);
-    echo json_encode(["message" => "Invalid department data"]);
-}
+    $departmentIsNumeric = is_numeric($data['department']);
+    if ($departmentIsNumeric) {
+        $data['department'] = (int)$data['department'];
+        $departmentExists = ValidationService::checkDepartmentExists($db, $data['department']);
+    } else {
+        $departmentExists = false;
+        http_response_code(400);
+        echo json_encode(["message" => "Invalid department data"]);
+    }
 
-$passedValidation =
-    $data['title']
-    && $data['name']
-    && $severityExists
-    && $departmentExists;
+    $passedValidation =
+        $data['title']
+        && $data['name']
+        && $severityExists
+        && $departmentExists;
 
-if (!$passedValidation) {
-    http_response_code(400);
-    echo json_encode(["message" => "Invalid issue data"]);
-    return;
-}
+    if (!$passedValidation) {
+        http_response_code(400);
+        echo json_encode(["message" => "Invalid issue data"]);
+        return;
+    }
 
-$newIssue = IssueHydrator::createIssue($db, $data);
+    $newIssue = IssueHydrator::createIssue($db, $data);
 
-if ($newIssue) {
-$output = [
+    $output = [
         'message' => "Issue created",
         'id' => $newIssue['id']
     ];
     http_response_code(201);
-} else {
+    echo json_encode($output);
+
+} catch (Exception $e) {
     $output = [
         'message' => "Unexpected error",
     ];
     http_response_code(500);
+    echo json_encode($output);
 }
-echo json_encode($output);
-
-
 
 
 
