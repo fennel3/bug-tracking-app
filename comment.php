eabe1c7469b7fe1fb3dc4dee2c61690eddcbb0d5
Liam O'Connell <liamslost@googlemail.com>
2024-11-08 13:51:29 +0000

    trainer edits

--- comment.php
+++ comment.php
@@ -10,43 +10,46 @@ use ITBugTracking\Hydrators\CommentHydrator;
 use ITBugTracking\Services\ValidationService;
 
 $db = DatabaseConnector::connect();
-$issue_id = $_GET['id'];
 
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
-
-try{
-$newIssue = CommentHydrator::createComment($db, $data, $issue_id);
-
-header('Content-Type: application/json; charset=utf-8');
-header("Access-Control-Allow-Origin: *");
-
-
-
-$data['name'] = ValidationService::validateStringInput($data['name'], 255);
-
-if (isset($data['comment'])) {
-    return $data['comment'] = ValidationService::validateStringInput($data['comment'], 65535);
-}
-
-$passedValidation =
-    $data['name']
-    && $data['comment'];
-
-if (!$passedValidation) {
+if (isset($_GET['id'])) {
+    $issue_id = $_GET['id'];
+} else {
     http_response_code(400);
     echo json_encode(["message" => "Invalid comment data"]);
     return;
 }
 
-if ($newIssue) {
-    $output = [
-        'message' => "Issue created",
-        'id' => $newIssue['id']
-    ];
-    http_response_code(201);
-    echo json_encode($output);
-}
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+try {
+    $newIssue = CommentHydrator::createComment($db, $data, $issue_id);
+
+    header('Content-Type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+
+    $data['name'] = ValidationService::validateStringInput($data['name'], 255);
+
+    $data['comment'] = ValidationService::validateStringInput($data['comment'], 65535);
+
+    $passedValidation =
+        $data['name'] != '' && $data['name']
+        && $data['comment'] != '' && $data['comment'];
+
+    if (!$passedValidation) {
+        http_response_code(400);
+        echo json_encode(["message" => "Invalid comment data"]);
+        return;
+    }
+
+    if ($newIssue) {
+        $output = [
+            'message' => "Issue created",
+            'id' => $newIssue['id']
+        ];
+        http_response_code(201);
+        echo json_encode($output);
+    }
 } catch (Exception $e) {
     $output = [
         'message' => "Unexpected error",
@@ -54,5 +57,3 @@ if ($newIssue) {
     http_response_code(500);
     echo json_encode($output);
 }
-
-
